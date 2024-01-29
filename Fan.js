d8e5512757bf6c05a8d12de263b69c41b0033c7a
KarriNikhil <92252302+KarriNikhil@users.noreply.github.com>
2024-01-29 22:58:14 +0530

    Fan.js

---
+++ Fan.js
@@ -0,0 +1,7 @@
+function on(){
+  alert("I am beginner web developer\nThis is a fun fan i designed\nBy Nikhil")
+  document.getElementById("star").className='on';
+}
+function off(){
+  document.getElementById("star").className='off';
+}
